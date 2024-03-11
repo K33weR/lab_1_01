440c2f7fad50764559b186d67938603602381012
K33weR <dmitriysit312@gmail.com>
2024-03-11 10:21:52 +0200

    Replace files inside Res2, add CMake_1, Res1, NewRes1.

---
+++ Res1/Project1/Project1.cpp
@@ -0,0 +1,31 @@
+﻿
+//Лабораторная работа №1
+//«Типы данных и операции»
+// Вариант 6
+
+
+#include <iostream>
+
+int main()
+{
+	// Целочисленный 
+	short sh; // 2 байта -2^15 - 2^15
+	unsigned short sh1; // 2 байта 0 - 2^16
+	int in; // 4 байта (-2)^31 - 2^31
+	unsigned int in1; // 4 байта 0 - 2^32
+	long long lg; // 8 байта (-2)^63 - 2^63
+	unsigned long long lg1; // 8 байта 0 - 2^64
+	// Вещественные 
+	float fl; // 4 байта 
+	double du; // 8 байта
+	
+
+	unsigned short er;
+	unsigned short rt;
+	std::cin >> sh1>>er>>rt;
+	float p = (sh1 + er + rt) / 2;
+	double s = sqrt(p * (p - sh1) * (p - er) * (p - rt));
+	std::cout << s;
+}
+   
+
---
+++ Res2/Lab_002.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+int main()
+{
+    unsigned char x = 7, invMask = 255; // 00000111
+    unsigned short i, bitChange, invertBits;
+    std::cout << "Enter the index of a bit of a num 0-7 (right to left): ";
+    std::cin >> i;
+    unsigned char mask = ~(1 << i);
+    bitChange = x & mask; // Change i bit to 0
+    invertBits = x ^ invMask;
+    std::cout << "Before bit change: " << int(x) << ", and after: " << bitChange << std::endl;
+    std::cout << "\n_/Additional exercises\\_\n";
+    std::cout << "1) Before inversion: " << int(x) << ", and after: " << invertBits << std::endl;
+
+    return 0;
+}
